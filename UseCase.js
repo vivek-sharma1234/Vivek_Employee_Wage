73937bee196be2d975f2fd49d1aa0a13ff204172
student 1 <viveksharma005959@gmail.com>
2024-12-04 15:40:38 +0530

    Vivek add: First UC2

--- UseCase.js
+++ UseCase.js
@@ -12,3 +12,37 @@ function checkAttendance() {
     }
   }
   checkAttendance();
+
+
+// Use case 2 Calculate the wages for part-time and full-time
+function calculateDailyWages() {
+    const partTimeHours = 4;
+    const fullTimeHours = 8;
+    const wagesPerHour = 20;
+  
+    // Generate the random number between 0 to 2
+    let employeeType = Math.floor(Math.random() * 3);
+    let workedHours = 0;
+  
+    switch (employeeType) {
+      case 0:
+        workedHours = 0;
+        console.log("No Time worked");
+        break;
+      case 1:
+        workedHours = partTimeHours;
+        console.log("Part Time Worked");
+        break;
+      case 2:
+        workedHours = fullTimeHours;
+        console.log("Full Time Worked");
+        break;
+      default:
+        workedHours = 0;
+        console.log("Invalid Output");
+    }
+  
+    let dailyWages = workedHours * wagesPerHour;
+    return { dailyWages, workedHours }; // Return both wages and hours
+  }
+  calculateDailyWages();
